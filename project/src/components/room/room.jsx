378e733869926bbb4883fe55a9d228bb3c394470
Andrew <8911102@gmail.com>
2021-07-12 09:47:58 +0500

    problem with double click necessety on back history button was fixed

--- project/src/components/room/room.jsx
+++ project/src/components/room/room.jsx
@@ -47,7 +47,7 @@ function Room(props) {
           <span>Premium</span>
         </div>}
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <Link to={`/hotels/${id}`}>
+        <Link>
           <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place" />
         </Link>
       </div>
