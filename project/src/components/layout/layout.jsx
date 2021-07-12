0b8934f5057f517970328f874c62fc5e72c2abcf
Andrew <8911102@gmail.com>
2021-07-12 11:27:57 +0500

    path was changed to Favorites in Login Link

--- project/src/components/layout/layout.jsx
+++ project/src/components/layout/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { AppRoute} from '../../const.js';
 
 function Layout (props) {
   const className = props.className;
@@ -19,7 +20,7 @@ function Layout (props) {
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <Link className="header__nav-link header__nav-link--profile" to="/">
+                  <Link className="header__nav-link header__nav-link--profile" to={ AppRoute.FAVORITES }>
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
