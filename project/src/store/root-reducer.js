e96808a51d17f6a13652ce7fa8f2f0b1ae90312a
Andrew <8911102@gmail.com>
2021-07-25 21:45:00 +0500

    user reducer was added

---
+++ project/src/store/root-reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+export const NameSpace = {
+  PLACES: 'PLACES',
+  COMMENTS: 'COMMENTS',
+  USER: 'USER',
+  PLACE: 'PLACE',
+  COMMENT: 'COMMENT',
+};
+
+export default combineReducers();
+
---
+++ project/src/store/user/user.js
@@ -0,0 +1,28 @@
+import { ActionType } from '../actions';
+
+import {
+  AuthorizationStatus,
+} from '../../const';
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_AUTHORIZATION_STATUS:
+      return {
+        ...state,
+        authorizationStatus: action.payload,
+      };
+    case ActionType.LOGOUT:
+      return {
+        ...state,
+        authorizationStatus: AuthorizationStatus.NO_AUTH,
+      };
+    default:
+      return state;
+  }
+};
+
+export { user };
