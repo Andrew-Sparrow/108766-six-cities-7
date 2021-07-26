a0c8f64be8f61ce8941401cc80f941635d5bb62b
Andrew <8911102@gmail.com>
2021-07-26 17:24:22 +0500

    refactoring - unneccessary lines were deleted

--- project/src/components/place/place-favorite-button.jsx
+++ project/src/components/place/place-favorite-button.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-
 import {addToFavorite} from '../../store/api-actions';
 
 function PlaceFavoriteButton(props) {
