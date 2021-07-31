cf0748df165df3b52f557b2b2b9cf3f3493d199e
Andrew <8911102@gmail.com>
2021-07-31 19:06:34 +0500

    mock folder was returned back

---
+++ project/src/mock/comments.js
@@ -0,0 +1,26 @@
+export const comments = [
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4.2,
+    'user': {
+      'avatar_url': 'img/avatar-max.jpg',
+      'id': 4,
+      'is_pro': false,
+      'name': 'John',
+    },
+  },
+  {
+    'comment': 'A quiet cozy and pictu by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 2,
+    'rating': 2.8,
+    'user': {
+      'avatar_url': 'img/avatar-max.jpg',
+      'id': 5,
+      'is_pro': false,
+      'name': 'Smith',
+    },
+  },
+];
