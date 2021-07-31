3c8c21bd11a3a312a990ed94c0124a7d98ca5884
Andrew <8911102@gmail.com>
2021-07-31 16:42:56 +0500

    file action.test.js was added

---
+++ project/src/store/actions.test.js
@@ -0,0 +1,22 @@
+import {
+  ActionType,
+  changeLoadingCommentProcessStatus,
+  changeIsCommentSendedSuccessfullyStatus,
+  showErrorCommentFormMessage,
+  changeCity,
+  loadPlaces,
+  loadNearbyPlaces,
+  removeNearbyPlaces,
+  loadComments,
+  removeComments,
+  changeAuthorizationStatus,
+  changeLogin,
+  changeFavorite,
+  logout,
+  resetFavorites,
+  redirectToRoute
+} from "./actions";
+
+describe('Actions', () => {
+  
+});
