6998de7cf7659b38facf6b13e163f286892aa295
Andrew <8911102@gmail.com>
2021-07-31 17:52:00 +0500

    first test for actions - action creator for change loading comment process status returns correct action

--- project/src/store/actions.test.js
+++ project/src/store/actions.test.js
@@ -1,22 +1,29 @@
 import {
   ActionType,
-  changeLoadingCommentProcessStatus,
-  changeIsCommentSendedSuccessfullyStatus,
-  showErrorCommentFormMessage,
-  changeCity,
-  loadPlaces,
-  loadNearbyPlaces,
-  removeNearbyPlaces,
-  loadComments,
-  removeComments,
-  changeAuthorizationStatus,
-  changeLogin,
-  changeFavorite,
-  logout,
-  resetFavorites,
-  redirectToRoute
-} from "./actions";
+  changeLoadingCommentProcessStatus
+  // changeIsCommentSendedSuccessfullyStatus,
+  // showErrorCommentFormMessage,
+  // changeCity,
+  // loadPlaces,
+  // loadNearbyPlaces,
+  // removeNearbyPlaces,
+  // loadComments,
+  // removeComments,
+  // changeAuthorizationStatus,
+  // changeLogin,
+  // changeFavorite,
+  // logout,
+  // resetFavorites,
+  // redirectToRoute
+} from './actions';
 
 describe('Actions', () => {
-  
+  it('action creator for change loading comment process status returns correct action', () => {
+    const expectedAction = {
+      type: ActionType.CHANGE_LOADING_COMMENT_PROCESS_STATUS,
+      payload: true,
+    };
+
+    expect(changeLoadingCommentProcessStatus(true)).toEqual(expectedAction);
+  });
 });
